54f554bf9dd889bf88017769d2dc06d3a5e37047
rmcmk <ryley.kimmel@live.com>
2024-02-20 10:55:09 -0500

    chore: tidy

--- src/main/kotlin/dev/rmcmk/gspm/gradle/module/GradleModuleToolingPlugin.kt
+++ src/main/kotlin/dev/rmcmk/gspm/gradle/module/GradleModuleToolingPlugin.kt
@@ -1,25 +1,24 @@
 package dev.rmcmk.gspm.gradle.module
 
+import javax.inject.Inject
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
-import javax.inject.Inject
 
 /**
- * A plugin which registers the [GradleModuleBuilder] with the Gradle
- * Tooling API.
+ * A plugin which registers the [GradleModuleBuilder] with the Gradle Tooling API.
  *
- * @param registry The registry to register the [GradleModuleBuilder] with.
  * @author Ryley Kimmel <me@rmcmk.dev>
  */
-abstract class GradleModuleToolingPlugin
-    @Inject
-    constructor(
-        private val registry: ToolingModelBuilderRegistry,
-    ) : Plugin<Project> {
-        override fun apply(target: Project) {
-            if (target == target.rootProject) {
-                registry.register(GradleModuleBuilder())
-            }
+abstract class GradleModuleToolingPlugin : Plugin<Project> {
+
+    /** The registry to register the [GradleModuleBuilder] with. */
+    @get:Inject
+    abstract val registry: ToolingModelBuilderRegistry
+
+    override fun apply(target: Project) {
+        if (target == target.rootProject) {
+            registry.register(GradleModuleBuilder())
         }
     }
+}
